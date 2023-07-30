668c836b72154760eac73bd456aedabfd11e881e
unknown <philipkurianv@gmail.com>
2023-07-30 01:20:22 -0400

    Ready to deploy changed nav issue

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Search from './Search'
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-800 p-4 top-3 flex justify-between flex-col md:flex-row sticky drop-shadow-lg">
+    <nav className="bg-slate-800 p-4 flex justify-between flex-col md:flex-row sticky drop-shadow-lg">
       <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href={"/"}>Tailwind Practice!</Link>
       </h1>
